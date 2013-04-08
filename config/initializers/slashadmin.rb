78da1ae48805ce4590a011bb7aca2c519156e6d9
Sergey Gridasov <grindars@gmail.com>
2013-04-08 12:10:40 +0400

    Enabled activeadmin compatibility.

--- config/initializers/slashadmin.rb
+++ config/initializers/slashadmin.rb
@@ -2,3 +2,5 @@ SlashAdmin::Engine.config.authentication_method = :authenticate_user!
 SlashAdmin::Engine.config.current_user_method   = :current_user
 SlashAdmin::Engine.config.restrict_model        = ->(model) { model.restrict(slashadmin_current_user) }
 SlashAdmin::Engine.config.unrestrict_model      = ->(model) { model.insecure }
+SlashAdmin::Engine.config.activeadmin_compat           = true
+

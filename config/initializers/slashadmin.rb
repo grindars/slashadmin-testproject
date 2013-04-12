b7ae010e1b7b8ab58b1208d1205c439a61d9ec40
Sergey Gridasov <grindars@gmail.com>
2013-04-12 10:41:16 +0400

    Updated initializer.

--- config/initializers/slashadmin.rb
+++ config/initializers/slashadmin.rb
@@ -1,6 +1,8 @@
-SlashAdmin::Engine.config.authentication_method = :authenticate_user!
-SlashAdmin::Engine.config.current_user_method   = :current_user
-SlashAdmin::Engine.config.restrict_model        = ->(model) { model.restrict(slashadmin_current_user) }
-SlashAdmin::Engine.config.unrestrict_model      = ->(model) { model.insecure }
-SlashAdmin::Engine.config.activeadmin_compat           = true
+SlashAdmin.configure do |c|
+  c.authentication_method = :authenticate_user!
+  c.current_user_method   = :current_user
+  c.restrict_model        = ->(model) { model.restrict(slashadmin_current_user) }
+  c.unrestrict_model      = ->(model) { model.insecure }
+  c.compatibility         = :active_admin
+end
 
